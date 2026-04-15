d84b825ece7fdce9dcd7a0e37ae29c9bdd5e98d2
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-04-15 12:34:14 +0530

    feat: fetch latest data from API
    - fetch latest data
    - manipulate those data and render latest statistics

--- js/index.js
+++ js/index.js
@@ -1,2 +1,36 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const downloadsCount = document.getElementById("downloadsCount");
+const paidUsersCount = document.getElementById("paidUsersCount");
+const imagesCount = document.getElementById("imagesCount");
+
+// Fetch data from API
+async function fetchStatistics() {
+  try {
+    const response = await fetch(
+      "https://www.greatfrontend.com/api/projects/challenges/statistics-metrics?latest=true",
+    );
+
+    if (!response.ok) {
+        throw new Error("HTTP error: " + response.status);
+    }
+
+    const result = await response.json();
+    return result.data;
+  } catch (error) {
+    console.log("ERROR: " + error);
+  }
+}
+
+async function main() {
+    const data = await fetchStatistics();
+
+    data.map(item => {
+        item.value = item.value.toLocaleString("en-US");
+    })
+
+    downloadsCount.innerText = data[0].value;
+    paidUsersCount.innerText = data[1].value;
+    imagesCount.innerText = data[2].value;
+}
+
+// Rendering statistics
+main();
